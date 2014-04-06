a5b87992ea3e36362db8c269e52c10b04e06657b
J. HOWARD SMART <hsmart@me.com>
2014-04-06 13:13:56 -0700

    Adding preferences TVC

---
+++ Smart Baby Tracker/SBTPreferencesTVC.h
@@ -0,0 +1,13 @@
+//
+//  SBTPreferencesTVC.h
+//  Smart Baby Tracker
+//
+//  Created by J. HOWARD SMART on 4/6/14.
+//  Copyright (c) 2014 J. HOWARD SMART. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBTPreferencesTVC : UITableViewController
+
+@end
---
+++ Smart Baby Tracker/SBTPreferencesTVC.m
@@ -0,0 +1,18 @@
+//
+//  SBTPreferencesTVC.m
+//  Smart Baby Tracker
+//
+//  Created by J. HOWARD SMART on 4/6/14.
+//  Copyright (c) 2014 J. HOWARD SMART. All rights reserved.
+//
+
+#import "SBTPreferencesTVC.h"
+
+@interface SBTPreferencesTVC ()
+
+@end
+
+@implementation SBTPreferencesTVC
+
+
+@end
