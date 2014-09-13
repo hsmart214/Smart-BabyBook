b31f471b17e4db76495bf1c4d29739b3890340f4
J. HOWARD SMART <hsmart@me.com>
2014-09-13 16:42:12 -0700

    Catching up

--- Smart Baby Tracker/SBTGraphViewController.m
+++ Smart Baby Tracker/SBTGraphViewController.m
@@ -171,6 +171,7 @@ static NSString * const SBTGraphCacheFilePrefix = @"com.mySmartSoftware.graphCac
     NSDictionary *attrs = @{NSFontAttributeName: font};
     
     // use the [self currentMeasureVisibleExtents] to decide where to put labels
+    //
     CGRect visibleMeasures = [self currentMeasureVisibleExtents];
     double maxAgeShown = visibleMeasures.origin.x + visibleMeasures.size.width;
     double maxMeasureShown = [self maxVRange] - visibleMeasures.origin.y;
@@ -182,6 +183,7 @@ static NSString * const SBTGraphCacheFilePrefix = @"com.mySmartSoftware.graphCac
                                                                      forAge:maxAgeShown
                                                                forParameter:self.parameter
                                                                   forGender:self.baby.gender];
+    
     for (int i = 0; i < [percentiles count]; i++){
         double measure = [pctMeasures[i] doubleValue];
         if (measure > minMeasureShown && measure < maxMeasureShown){
@@ -198,10 +200,10 @@ static NSString * const SBTGraphCacheFilePrefix = @"com.mySmartSoftware.graphCac
     // TODO: Draw the axes
     // add the ticks and labels for the data (y axis) remember the y axis is inverted
     // add the ticks and labels for the age (x axis)
+    // add the percentile marks to the ends of the percentile lines (left edge)
     
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     [(UIImageView *)[self.overlayView.subviews firstObject] setImage: image];
-    // TODO: Add constraints so the image view sticks to the bounds of the overlay view
     UIGraphicsEndImageContext();
 }
 
