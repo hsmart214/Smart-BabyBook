0f6a47df73c068be7e7ea443584238ba9b35bfa5
J. HOWARD SMART <hsmart@me.com>
2016-01-03 14:33:24 -0800

    Secure all files

---
+++ Smart Baby Tracker/Smart Baby Tracker-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Smart Baby Tracker-Bridging-Header.h
+//  Smart BabyBook
+//
+//  Created by J. HOWARD SMART on 1/3/16.
+//  Copyright © 2016 J. HOWARD SMART. All rights reserved.
+//
+
+#ifndef Smart_Baby_Tracker_Bridging_Header_h
+#define Smart_Baby_Tracker_Bridging_Header_h
+
+#include "Smart BabyBook-Prefix.pch"
+#include "SBTBaby.h"
+
+#endif /* Smart_Baby_Tracker_Bridging_Header_h */
